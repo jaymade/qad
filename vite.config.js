07dd56ec7171bfaf885a955135de2b5982b7c765
Jay Rich <jaymade@Clyde.localdomain>
2026-05-24 11:33:16 -0400

    fix: add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-// import ghPages from 'vite-plugin-gh-pages'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  base: "/qad/",
+});
